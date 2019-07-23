4759d490d6c8715135a5fd903182dcb513bb52d5
Ivan Morales <imoraleschazam@gmail.com>
2019-07-23 10:17:17 -0500

    Pipes Angular
    Version 1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  nombre = "Ivan";
+  nombre2 = "IvAn mOraLes cRuz";
+  pass = "Ivan2019";
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    Nombre: 'Peter Parker',
+    Clave: 'Hombre Araña',
+    Edad: '35',
+    Direccion: {
+
+    Ciudad: 'New York',
+    Casa: '20'
+    }
+  };
+
+valorDePromesa = new Promise( ( resolve, reject )=>{
+  setTimeout( ()=>resolve('llego la data!'), 3500 );
+});
+
+fecha = new Date();
+
+video = "bYsebQKLyMQ";
+
+activar:boolean = true;
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+// LOCALE ID nos permitira definir la localidad para fechas
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// Para mostrar fechas español se deben importar los siguientes datos:
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
+
+import {  CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  // por ultimo añadir en providers el id de nuestra locazion
+  providers: [ {provide: LOCALE_ID, useValue: 'es'} ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
